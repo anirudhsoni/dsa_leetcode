050fc4f90384eb8bb887697ea340ec771921ccf0
anirudhsoni <38383377+anirudhsoni@users.noreply.github.com>
2024-12-26 23:33:47 +0530

    Time: 206 ms (53.87%) | Memory: 27.9 MB (5.31%) - LeetSync

--- 3595-rearrange-k-substrings-to-form-target-string/rearrange-k-substrings-to-form-target-string.py
+++ 3595-rearrange-k-substrings-to-form-target-string/rearrange-k-substrings-to-form-target-string.py
@@ -1,12 +1,9 @@
 class Solution:
     def isPossibleToRearrange(self, s: str, t: str, k: int) -> bool:
-        subs={}
-        ln=len(s)//k
-        for i in range(0,len(s),ln):
-            subs[tuple(s[i:i+ln])]=subs.get(tuple(s[i:i+ln]),0)+1
-        for i in range(0,len(s),ln):
-            if tuple(t[i:i+ln]) not in subs or subs[tuple(t[i:i+ln])]==0:
-                return False
-            else:
-                subs[tuple(t[i:i+ln])]-=1
-        return True
+        size = len(s)//k
+        parts = [s[i:i+size] for i in range(0, len(s)-size+1, size)]
+        s_counter = Counter(parts)
+        parts_d = [t[i:i+size] for i in range(0, len(t)-size+1, size)]
+        t_counter = Counter(parts_d)
+        return s_counter == t_counter
+        
