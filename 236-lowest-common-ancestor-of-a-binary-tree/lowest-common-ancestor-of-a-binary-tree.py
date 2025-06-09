da5abeae02a67f75e9216573049ff51bfaed988f
anirudhsoni <38383377+anirudhsoni@users.noreply.github.com>
2025-06-09 11:08:41 +0530

    Time: 43 ms (96.13%) | Memory: 22.3 MB (21.45%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
@@ -7,40 +7,19 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        pPath=[]
-        qPath=[]
+        if root is None:
+            return None
+        if root.val == p.val or root.val == q.val:
+            return root
 
-        def getPathP(n):
-            if not n:
-                return
-            pPath.append(n)
-            if pPath[-1]!=p:
-                getPathP(n.left)
-                getPathP(n.right)
-                if pPath[-1]!=p:
-                    pPath.pop()
-        def getPathQ(n):
-            if not n:
-                return
-            qPath.append(n)
-            # print(qPath) 
-            if qPath[-1]!=q:
-                getPathQ(n.left)
-                getPathQ(n.right)
-                if qPath[-1]!=q:
-                    qPath.pop()
-        getPathP(root)
-        getPathQ(root)
-        # for i in pPath:
-        #     print(i.val)
-        # # print('xXx')
-        # for i in qPath:
-        #     print(i.val)
-        lca=pPath[0]
-        i=0
-        while i<len(pPath) and i<len(qPath) and pPath[i]==qPath[i]:
-            i+=1
-            lca=pPath[i-1]
-        return lca
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
 
-        
+        if left and right:
+            return root
+        if left:
+            return left
+        if right:
+            return right
+
+        return None
